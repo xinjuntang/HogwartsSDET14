519c7060b599617bc29e227e477fcd14722e08fe
tangxinjun <624354119@qq.com>
2020-08-24 23:34:07 +0800

    home pc code commit

---
+++ pythoncode/calc.py
@@ -0,0 +1,19 @@
+#!-*- coding:utf-8 -*-
+
+
+class Calc:
+    def add(self, a, b):
+        c = a + b
+        return c
+
+    def minus(self, a, b):
+        c = a - b
+        return c
+
+    def mul(self, a, b):
+        c = a * b
+        return c
+
+    def div(self, a, b):
+        c = a / b
+        return c
