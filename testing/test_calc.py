f2055bdafcbb793269b6366ce372647ffbab3fb3
tangxinjun <624354119@qq.com>
2020-09-02 22:49:16 +0800

    data driver by yaml for all cases

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -5,8 +5,11 @@ from utils import utils
 
 
 class TestCalc:
-    path = './testdata/calc_datas'
-    data = utils.yamlload(path)
+    path_add = './testdata/calc_datas'
+    path_mul = './testdata/mul_data'
+    data_add = utils.yamlload(path_add)
+    data_mul = utils.yamlload(path_mul)
+
 
     def setup_class(self):
         self.cal = Calc()
@@ -15,13 +18,24 @@ class TestCalc:
     def teardown_class(self):
         print("test finished.")
 
-    @pytest.mark.parametrize('a, b, ans', data)
+    @pytest.mark.parametrize('a, b, ans', data_add)
     def test_add(self, a, b, ans):
         c = self.cal.add(a, b)
         assert ans == c
 
+    @pytest.mark.parametrize('a, b, ans', data_add)
     def test_minus(self, a, b, ans):
-        c = self.cal.minus(a, b)
+        c = self.cal.minus(ans, b)
+        assert a == c
+
+    @pytest.mark.parametrize('a, b, ans', data_mul)
+    def test_multi(self, a, b, ans):
+        c = self.cal.mul(a, b)
         assert ans == c
+
+    @pytest.mark.parametrize('a, b, ans', data_mul)
+    def test_div(self, a, b, ans):
+        c = self.cal.div(ans, b)
+        assert a == c
 
 
